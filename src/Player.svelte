01c9380dd2ddce398794fe4e3e6a91d62f81f162
chrisdaid <c.chrisdb@gmail.com>
2022-01-24 22:50:54 -0500

    added files

---
+++ src/Player.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+	export let name;
+	export let points;
+	let showControls = false;
+
+	const addPoint = () => points++;
+	const removePoint = () => points--;
+	const toggleControls = () => (showControls = !showControls);
+</script>
+
+	<style>
+		h1 {
+			color: #204f6e;
+		}
+
+		h3 {
+			margin-bottom: 10px;
+		}
+	</style>
+
+	<div class="card">
+		<h1>
+			Hello {name}!
+			<button class="btn btn-sm" on:click={toggleControls}>
+			{#if showControls}-{:else}+{/if}
+			</button>
+      <button class="btn btn-danger" on:click={removePlayer}>Delete Player</button>
+
+		</h1>
+		<h3>Points: {points}</h3>
+		{#if showControls}
+		<button class="btn" on:click={addPoint}>+1</button>
+		<button class="btn btn-dark" on:click={removePoint}>-1</button>
+		<input type="number" bind:value={points}>
+		{/if}
+	</div>
